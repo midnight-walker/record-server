53b35f3d373b84a62b6c3e567cec1d867731d35d
tangqiaojie <tangqiaojie@zbj.com>
2017-11-30 14:26:14 +0800

    dev

--- route/index.js
+++ route/index.js
@@ -2,7 +2,9 @@
  * Created by tqj <2482366539@qq.com> on 2017/8/7.
  */
 var index = async (ctx, next) => {
-    console.log(ctx);
+    console.log(ctx.req);
+    console.log(ctx.req.params);
+    ctx.rest(123);
 };
 
 var auth = async (ctx, next) => {
