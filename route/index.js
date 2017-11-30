eb64c2224f17aa560584dfdc163b8bbc60d9e115
tangqiaojie <tangqiaojie@zbj.com>
2017-11-30 14:15:12 +0800

    dev

--- route/index.js
+++ route/index.js
@@ -2,9 +2,15 @@
  * Created by tqj <2482366539@qq.com> on 2017/8/7.
  */
 var index = async (ctx, next) => {
+    console.log(ctx);
+};
+
+var auth = async (ctx, next) => {
 
 };
 
+
 module.exports = {
-    'GET /': index
+    'GET /': index,
+    'GET /auth': auth,
 };
