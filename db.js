4ca0d863a97887c6864016c440c798ad3d6fb69a
tangqiaojie <tangqiaojie@zbj.com>
2017-12-09 11:10:36 +0800

    dev

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 
 const uuid = require('node-uuid');
 
-const config = require('./db-config');
+const config = require('./config/db');
 
 console.log('init sequelize...');
 
